74b305b512bddf8b2fbf2783d7bbb9cc63516563
Muhazam-Mustapha <30557687+Muhazam-Mustapha@users.noreply.github.com>
2022-12-20 21:11:27 +0800

    The first commit
    Mostly copied from the GUI counterpart

---
+++ FractionLib/FractionLib/FractionLib/ConsProperties.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FractionLib
+{
+    /// <summary>
+    /// The class Fraction, meant to represent a fraction as in math.
+    /// </summary>
+    public partial class Fraction
+    {
+        private int _numerator;
+        private int _denominator;
+
+        /// <summary>
+        /// The numerator of the fraction
+        /// </summary>
+        public int Numerator {
+            get { return _numerator; }
+            set { _numerator = value; } }
+
+        /// <summary>
+        /// The denominator of the fraction
+        /// </summary>
+        public int Denominator {
+            get { return _denominator; }
+            set { _denominator = value; } }
+
+        /// <summary>
+        /// The default no parameter constructor
+        /// </summary>
+        public Fraction()
+        {
+            _numerator = 0;
+            _denominator = 1;
+        }
+
+        /// <summary>
+        /// The two parameter constructor with numerator and denominator
+        /// </summary>
+        /// <param name="num">The numerator</param>
+        /// <param name="den">The denominator</param>
+        public Fraction(int num, int den)
+        {
+            _numerator = num;
+            _denominator = den;
+        }
+    }
+}
---
+++ FractionLib/FractionLib/FractionLib/Methods.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FractionLib
+{
+    public partial class Fraction
+    {
+        private int GCD()
+        {
+            int a = _numerator;
+            int b = _denominator;
+
+            while (a != b) {
+                if (a > b) a -= b;
+                else       b -= a; }
+
+            return a;
+        }
+
+        /// <summary>
+        /// Divides the numerator and denominator with their GCD to
+        /// get smaller version of the fraction.
+        /// </summary>
+        public void Normalize()
+        {
+            int gcd = GCD();
+
+            _numerator /= gcd;
+            _denominator /= gcd;
+        }
+
+        /// <summary>
+        /// Gives the equivalent decimal value of the fraction.
+        /// </summary>
+        /// <returns>The equivalent decimal value</returns>
+        public double Value()
+        {
+            return ((double)_numerator) / _denominator;
+        }
+
+        /// <summary>
+        /// Method to honor the creator of this library.
+        /// </summary>
+        /// <returns>The About string</returns>
+        public static string About()
+        {
+            return "Fraction library, created by Muhazam Mustapha\n" +
+                   "January 2023\n" +
+                   "Go to GitHub: \n" +
+                   "for more information.";
+        }
+
+        /// <summary>
+        /// Gives the string equivalent of the fraction for printing purposes.
+        /// </summary>
+        /// <returns>The string equivalent of the fraction</returns>
+        public string ToString()
+        {
+            return _numerator.ToString()+"/"+_denominator.ToString();
+        }
+    }
+}
